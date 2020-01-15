b291d1f57a659180ada6d472d9a26fae3a8dd2b3
Thom Wheeler <wheeler2501@gmail.com>
2020-01-15 12:38:38 +0100

    Updates

---
+++ precache-manifest.57703319f477ec4aacd4638d938133f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63ecc10f741f5ef14b4c542d2fbfd0c",
+    "url": "/pokesearch/app/index.html"
+  },
+  {
+    "revision": "d8ce60ff70562cfdf370",
+    "url": "/pokesearch/app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6180cfe7d46b648a8755",
+    "url": "/pokesearch/app/static/js/2.1d9e0624.chunk.js"
+  },
+  {
+    "revision": "0eff8c8268fcb46278ccae785a98005d",
+    "url": "/pokesearch/app/static/js/2.1d9e0624.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d8ce60ff70562cfdf370",
+    "url": "/pokesearch/app/static/js/main.ae6322a0.chunk.js"
+  },
+  {
+    "revision": "44f1239f0c389b7194c3",
+    "url": "/pokesearch/app/static/js/runtime-main.e959e2db.js"
+  }
+]);
