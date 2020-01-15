fb8c5f4c459320222f1de0c11b997280cdc23a7e
Thom Wheeler <wheeler2501@gmail.com>
2020-01-15 14:59:37 +0100

    Updates

---
+++ precache-manifest.c45242e58110a65f23814e756362cd20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ac355554f682529b999ebe6890784c",
+    "url": "/pokesearch/index.html"
+  },
+  {
+    "revision": "18ceac87762fe79623fa",
+    "url": "/pokesearch/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7fd79d57f716254c6d50",
+    "url": "/pokesearch/static/js/2.8032fce6.chunk.js"
+  },
+  {
+    "revision": "0eff8c8268fcb46278ccae785a98005d",
+    "url": "/pokesearch/static/js/2.8032fce6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18ceac87762fe79623fa",
+    "url": "/pokesearch/static/js/main.e258de0d.chunk.js"
+  },
+  {
+    "revision": "272e4cf48dc11d2f06cd",
+    "url": "/pokesearch/static/js/runtime-main.4ba042a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokesearch/app/precache-manifest.57703319f477ec4aacd4638d938133f7.js"
+  "/pokesearch/precache-manifest.c45242e58110a65f23814e756362cd20.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokesearch/app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokesearch/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
